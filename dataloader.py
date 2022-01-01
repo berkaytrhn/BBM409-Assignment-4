3c4816b0907cd4422479b9b0c741122bec41fc2a
berkaytrhn <berkaytrhn@gmail.com>
2022-01-01 19:07:59 +0300

    part1 completed

--- dataloader.py
+++ dataloader.py
@@ -2,14 +2,15 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm 
+from sklearn.model_selection import train_test_split
 
 class DataLoader:
 
-    def __init__(self, path, batch_size, classes=None):
-        self.batch_size = batch_size
+    def __init__(self, path, size, classes=None):
         self.path = path
         self.classes = classes if classes else os.listdir(path)
         self.classes = {index:_class for index,_class in enumerate(self.classes)}
+        self.img_height, self.img_width=size
 
         print(self.classes)
 
@@ -23,10 +24,12 @@ class DataLoader:
             counter=1
             _path = os.path.join(self.path, _class)
             for image in tqdm(os.listdir(_path)):
-                if counter%250==0:
+                """
+                if counter%30==0:
                     break
+                """
                 img_path = os.path.join(_path, image)
-                img = cv2.resize(cv2.imread(img_path, 0),(128, 128))
+                img = cv2.resize(cv2.imread(img_path, 0),(self.img_height, self.img_width))
                 img = img.flatten()/255.0
                 images.append(img)
                 labels.append(_class_index)
@@ -36,10 +39,34 @@ class DataLoader:
         print(images.shape)
         
         return (images, labels)
-            
-
     
-    def preprocess(self):
-        for _class in self.classes:
-            _path = os.path.join(self.path, _class)
-            print(f"number of pictures belongs to {_class} -> {len(os.listdir(_path))}")
+    def batch_and_split_process(self, images, labels, batch_size, test_size=0.2, valid_size=0.2):
+        self.batch_size=batch_size
+        # test set
+        X_train, X_test, y_train, y_test = train_test_split(images, labels, shuffle=True, test_size=test_size)
+
+        # validation set
+        X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, shuffle=True, test_size=valid_size)
+        
+
+        sets = [X_train, y_train]
+
+        batched_sets = []
+
+        for _set in sets:
+            _shape = _set.shape
+            _step = _shape[0]//self.batch_size
+            batched=[]
+            for i in range(_step+1):
+                batched.append(_set[i*self.batch_size:(i+1)*self.batch_size])
+            batched = np.array(batched)
+            #print(batched.shape)
+            batched_sets.append(batched)
+            """
+            for i in batched:
+                print("----")
+                print(i.shape)
+                print(i)
+            """
+        batched_sets = np.array(batched_sets)
+        return np.array([batched_sets[0], X_valid, X_test, batched_sets[1], y_valid, y_test])
--- main.py
+++ main.py
@@ -1,66 +1,88 @@
 import numpy as np
 import argparse
+
+from sklearn.utils import validation
 from neural_network import NeuralNetwork
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from dataloader import DataLoader
 from utils import *
+from itertools import product
 
+def multi_experiment(batch_sizes, learning_rates, hidden_layers, data, dataloader):
+    images, labels = data
+    num_classes = len(np.unique(labels))
+    prdct = list(product(batch_sizes, learning_rates, hidden_layers))
+    epochs=25
+    
+    
+    choosing_best = []
+
+    for parameters in prdct:
+        
+        batch_size = parameters[0]
+        lr = parameters[1]
+        hidden_layer = parameters[2]
+        print(batch_size, lr, hidden_layer)
+        X_train, X_valid, X_test, y_train, y_valid, y_test = dataloader.batch_and_split_process(images, labels, batch_size, test_size=0.2, valid_size=0.2)
+
+        nn = NeuralNetwork(X_train, y_train, hidden_layer_size=hidden_layer, output_size=num_classes)
+
+        history = nn.fit(epochs, lr, validation_set=(X_valid, y_valid), save_every=10)
+        accuracy = history["accuracy"]
+        loss = history["loss"]
+
+        save_graph("graphs", accuracy, "Accuracies", "Accuracy", f"acc_{lr}lr_{batch_size}bs_{hidden_layer}hl.png")
+        save_graph("graphs", loss, "Losses", "Loss", f"loss_{lr}lr_{batch_size}bs_{hidden_layer}hl.png")
+
+        pred = nn.predict(X_test)
+        test_acc = round(accuracy_score(y_test, pred)*100, 5)
+        choosing_best.append()
+        print(f"\nTest accuracy: {test_acc}")
+
+# mainde save plot dene sonra multi experiment çalıştır
 def main(args):
     np.warnings.filterwarnings("ignore")
 
-    data = sns.load_dataset("iris")
     
-    data["species"] = LabelEncoder().fit_transform(data["species"])
     BATCH_SIZE=32
-    dataloader = DataLoader("./dataset", BATCH_SIZE)
-    #dataloader.preprocess()
-    image_X, image_y = dataloader.load_images()
+    HEIGHT=32
+    WIDTH=32
+    HIDDEN_LAYERS=2
+    EPOCHS = 50
+    LEARNING_RATE = 0.01
     
-    """
-    data = np.array(data)
-    np.random.shuffle(data)
-    X = data[:,:-1]
-    y = data[:,-1].astype(int)
-    """
+    
+    dataloader = DataLoader("./dataset", size=(HEIGHT, WIDTH))
+    images, labels = dataloader.load_images()
+    num_classes = len(np.unique(labels))
+    X_train, X_valid, X_test, y_train, y_valid, y_test = dataloader.batch_and_split_process(images, labels, BATCH_SIZE, test_size=0.2, valid_size=0.2)
 
-
-    # test set
-    X_train, X_test, y_train, y_test = train_test_split(image_X, image_y, shuffle=True, test_size=0.2)
-
-    # validation set
-    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, shuffle=True, test_size=0.2)
-
-
-
-
-    nn = NeuralNetwork(X_train, y_train)
-    #nn = NeuralNetwork(image_X, image_y)
-
+    nn = NeuralNetwork(X_train, y_train, hidden_layer_size=HIDDEN_LAYERS, output_size=num_classes)
 
     if args.load_weights:
-        nn.load_weights(["weights1.npy", "weights2.npy"])
-
-
-
+        nn.load_weights()
     else:
-        EPOCHS = 10000
-        LEARNING_RATE = 0.0004
+        
 
-        history = nn.fit(EPOCHS, LEARNING_RATE, validation_set=(X_valid, y_valid))
+        history = nn.fit(EPOCHS, LEARNING_RATE, validation_set=(X_valid, y_valid), save_every=10)
         #history = nn.fit(EPOCHS, LEARNING_RATE)
         accuracy = history["accuracy"]
         loss = history["loss"]
         plot_graph(accuracy, "Accuracies", "Accuracy")
         plot_graph(loss, "Losses", "Loss")
-
+        save_graph("graphs", accuracy, "Accuracies", "Accuracy", f"acc_{LEARNING_RATE}lr_{BATCH_SIZE}bs_{HIDDEN_LAYERS}hl.png")
         nn.save_weights()
 
     pred = nn.predict(X_test)
     print(f"\nTest accuracy: {round(accuracy_score(y_test, pred)*100, 5)}")
 
+
+
+    if args.experiment:
+        batch_sizes = [16, 32, 64, 128]
+        learning_rates = [0.005, 0.010, 0.015, 0.020]
+        hidden_layers = [0, 1, 2]
+        multi_experiment(batch_sizes, learning_rates, hidden_layers, [images, labels], dataloader)
 
 
 
@@ -70,7 +92,8 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--load_weights", default=None)
+    parser.add_argument("--load_weights", default=None, action="store_true")
+    parser.add_argument("--experiment", default=None, action="store_true")
 
 
     args = parser.parse_args()
